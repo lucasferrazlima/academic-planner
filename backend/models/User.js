6585fb5d380e4794e9f7fa1f3e193204aa1b71ac
Lucas <lucasferrazl@hotmail.com>
2023-04-17 17:02:42 -0300

    Add User model

---
+++ backend/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  tasks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Task',
+    },
+  ],
+});
+
+userSchema.plugin(uniqueValidator);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
